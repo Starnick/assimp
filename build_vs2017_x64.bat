3b1754cb86b7ec986da9a9d65641ade523b3a748
Nicholas Woodfield <nicholas.woodfield@gmail.com>
2020-08-13 22:43:05 -0400

    Added a debug configuration to the win64 build setup

--- build_vs2017_x64.bat
+++ build_vs2017_x64.bat
@@ -4,3 +4,7 @@ SET GENERATOR=Visual Studio 15 2017
 SET VSPROJECT_DIR="./vsprojects/x64"
 cmake CMakeLists.txt -G "%GENERATOR% Win64" -S %SOURCE_DIR% -B %VSPROJECT_DIR% -DLIBRARY_SUFFIX=""
 cmake --build %VSPROJECT_DIR% --config release
+
+SET VSPROJECT_DIR="./vsprojects/x64_debug"
+cmake CMakeLists.txt -G "%GENERATOR% Win64" -S %SOURCE_DIR% -B %VSPROJECT_DIR% -DLIBRARY_SUFFIX=""
+cmake --build %VSPROJECT_DIR% --config debug
